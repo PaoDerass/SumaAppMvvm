b74531de2253eb63e1ecc3b13b64c47dbb1c95e1
PaoDerass <paola_deras@unitec.edu>
2024-09-18 21:49:22 -0600

    suma view model

--- ViewModel/SumaVM.cs
+++ ViewModel/SumaVM.cs
@@ -1,12 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Windows.Input;
 
 namespace SumaAppMvvm.ViewModel
 {
-    internal class SumaVM
+    public partial class SumaViewModel : ObservableObject
     {
+        [ObservableProperty]
+        private string numero1;
+
+        [ObservableProperty]
+        private string numero2;
+
+        [ObservableProperty]
+        private string resultado;
+
+        public SumaViewModel()
+        {
+            LimpiarCommand = new RelayCommand(Limpiar);
+            SumarCommand = new RelayCommand(Sumar);
+        }
+
+        public ICommand SumarCommand { get; }
+        public ICommand LimpiarCommand { get; }
+
+        private void Sumar()
+        {
+            if (string.IsNullOrWhiteSpace(Numero1) || string.IsNullOrWhiteSpace(Numero2))
+            {
+                Resultado = "Los campos son obligatorios";
+                return;
+            }
+
+            if (double.TryParse(Numero1, out double num1) && double.TryParse(Numero2, out double num2))
+            {
+                Resultado = $"Resultado: {num1 + num2}";
+            }
+            else
+            {
+                Resultado = "ingrese solo numeros validos";
+            }
+        }
+
+        private void Limpiar()
+        {
+            Numero1 = string.Empty;
+            Numero2 = string.Empty;
+            Resultado = string.Empty;
+        }
     }
 }
